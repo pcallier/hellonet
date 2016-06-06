f18f16fcbe1fef7b0dfa803005d93f40d429411f
Patrick Callier <pat.r.callier@gmail.com>
2016-06-05 22:41:24 -0700

    Stub out TF LSTM

--- nets.py
+++ nets.py
@@ -2,3 +2,16 @@
 """
 
 """
+
+import tensorflow as tf
+import chainer
+
+import data
+
+
+def tf_lstm(input):
+    vocab_size = len(data.default_vocab)
+    lstm_size = 10
+    lstm = tf.nn.rnn_cell.BasicLSTMCell(self, lstm_size,
+                                        input_size=(lstm_size, vocab_size))
+    
